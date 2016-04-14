a61789659c9075a4d37fe497dd754fd404a862e7
joanlopez <joanjan14@gmail.com>
2016-04-14 16:19:03 +0200

    Added security

--- main.js
+++ main.js
@@ -11,10 +11,7 @@ if(Meteor.isClient) {
 	Template.main.events({
 		"submit .new-todo": function(event) {
 			var text = event.target.text.value;
-			Todos.insert({
-				text: text,
-				createdAt: new Date()
-			});
+			Meteor.call('addTodo', text);
 
 			// Clear form
 			event.target.text.value = '';
@@ -23,13 +20,55 @@ if(Meteor.isClient) {
 			return false;
 		},
 		"click .toggle-checked": function(event) {
-			Todos.update(this._id, {$set:{checked: !this.checked}});
+			Meteor.call('setChecked', this._id, !this.checked);
 		},
 		"click .delete-todo": function(event) {
 			if(confirm('Are you sure?')) {
-				Todos.remove(this._id);	
+				Meteor.call('deleteTodo', this._id);	
 			}
 		}
 
 	});
+
+	accounts.ui.config({
+		passwordSignupFields: 'USERNAME ONLY'
+	});
 }
+
+if(Meteor.isServer) {
+	Meteor.publish('todos', function() {
+		if(!this.userId) {
+			return Todos.find();
+		} else {
+			return Todos.find({userId: this.userId});
+		}
+	});
+}
+
+Meteor.methods({
+	addTodo: function(text) {
+		if(!Meteor.userId()) {
+			throw new Meteor.Error('Not-authorized')
+		}
+		Todos.insert({
+			text: text,
+			createdAt: new Date(),
+			userId: Meteor.userId(),
+			username: Meteor.user().username
+		});
+	},
+	deleteTodo: function(todoId) {
+		var todo = Todos.findOne(todoId);
+		if(todo.userId !== Meteor.userId()) {
+			throw new Meteor.Error('Not-authorized');
+		}
+		Todos.remove(todoId);
+	},
+	setChecked: function(todoId, setChecked) {
+		var todo = Todos.findOne(todoId);
+		if(todo.userId !== Meteor.userId()) {
+			throw new Meteor.Error('Not-authorized');
+		}
+		Todos.update(todoId, {$set:{checked: setChecked}});
+	}
+});
