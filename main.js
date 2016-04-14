3054557c25fea772391e466752a052bf9021a579
joanlopez <joanjan14@gmail.com>
2016-04-14 12:05:35 +0200

    New features added: creation, edition, deletion

--- main.js
+++ main.js
@@ -4,11 +4,32 @@ if(Meteor.isClient) {
 	//Template helpers
 	Template.main.helpers({
 		todos: function() {
-			return Todos.find();
+			return Todos.find({}, {sort: {createdAt: -1}});
 		}
 	});
-}
 
-if(Meteor.isServer) {
+	Template.main.events({
+		"submit .new-todo": function(event) {
+			var text = event.target.text.value;
+			Todos.insert({
+				text: text,
+				createdAt: new Date()
+			});
 
+			// Clear form
+			event.target.text.value = '';
+
+			// Prevent submit
+			return false;
+		},
+		"click .toggle-checked": function(event) {
+			Todos.update(this._id, {$set:{checked: !this.checked}});
+		},
+		"click .delete-todo": function(event) {
+			if(confirm('Are you sure?') {
+				Todos.remove(this._id);	
+			}
+		}
+
+	});
 }
